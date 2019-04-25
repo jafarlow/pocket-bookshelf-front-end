2ee92abf730db8ed67e3c83b6bffc4209d78498f
jafarlow <jacobafarlow@gmail.com>
2019-04-25 16:49:52 -0400

    Setup base JS files
    Created Album and Auth directories
    Added api/events/ui files to each directory
    Added handlers and require links to app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,8 @@
 'use strict'
 
+//REQUIRE LINKS
+const userAuth = require("./auth/events.js")
+const userEvents = require("./album/events.js")
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
@@ -8,4 +11,6 @@
 
 $(() => {
   // your JS code goes here
+  userAuth.addHandlers()
+  userEvents.addHandlers()
 })
